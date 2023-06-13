e7055870b9a10ea6b5d5df82d8543e286a67b706
luisRiofrio <lriofrio915@gmail.com>
2023-06-13 18:03:49 -0400

    cart buttons

--- components/ProductBox.js
+++ components/ProductBox.js
@@ -75,7 +75,7 @@ export default function ProductBox({_id,title,description,price,images}) {
             ${price}
           </Price>
           <Button block onClick={() => addProduct(_id)} primary outline>
-            Agregar al carrito
+            Aañidir al carrito
           </Button>
         </PriceRow>
       </ProductInfoBox>
